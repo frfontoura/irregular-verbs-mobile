6b6ff71cbf795a6b1fa55736e15b135331f50478
frfontoura <frfontoura@gmail.com>
2019-07-05 19:52:14 -0300

    Primeira parte da lista

---
+++ src/assets/list_irreguar_verbs.js
@@ -0,0 +1,224 @@
+const verbs = [
+  {
+    baseForm: 'be',
+    simplePast: 'was, were',
+    pastParticiple: 'been',
+  },
+  {
+    baseForm: 'bear',
+    simplePast: 'bore',
+    pastParticiple: 'borne, born',
+  },
+  {
+    baseForm: 'beat',
+    simplePast: 'beat',
+    pastParticiple: 'beaten, beat',
+  },
+  {
+    baseForm: 'become',
+    simplePast: 'became',
+    pastParticiple: 'become',
+  },
+  {
+    baseForm: 'begin',
+    simplePast: 'began',
+    pastParticiple: 'begun',
+  },
+  {
+    baseForm: 'behold',
+    simplePast: 'beheld',
+    pastParticiple: 'beheld',
+  },
+  {
+    baseForm: 'bend',
+    simplePast: 'bent',
+    pastParticiple: 'bent',
+  },
+  {
+    baseForm: 'bet',
+    simplePast: 'bet',
+    pastParticiple: 'bet',
+  },
+  {
+    baseForm: 'bid',
+    simplePast: 'bid',
+    pastParticiple: 'bid',
+  },
+  {
+    baseForm: 'bind',
+    simplePast: 'bound',
+    pastParticiple: 'bound',
+  },
+  {
+    baseForm: 'bite',
+    simplePast: 'bit',
+    pastParticiple: 'bitten',
+  },
+  {
+    baseForm: 'bleed',
+    simplePast: 'bled',
+    pastParticiple: 'bled',
+  },
+  {
+    baseForm: 'blow',
+    simplePast: 'blew',
+    pastParticiple: 'blown',
+  },
+  {
+    baseForm: 'break',
+    simplePast: 'broke',
+    pastParticiple: 'broken',
+  },
+  {
+    baseForm: 'breed',
+    simplePast: 'bred',
+    pastParticiple: 'bred',
+  },
+  {
+    baseForm: 'bring',
+    simplePast: 'brought',
+    pastParticiple: 'brought',
+  },
+  {
+    baseForm: 'broadcast',
+    simplePast: 'broadcast',
+    pastParticiple: 'broadcast',
+  },
+  {
+    baseForm: 'build',
+    simplePast: 'built',
+    pastParticiple: 'built',
+  },
+  {
+    baseForm: 'buy',
+    simplePast: 'bought',
+    pastParticiple: 'bought',
+  },
+  {
+    baseForm: 'cast',
+    simplePast: 'cast',
+    pastParticiple: 'cast',
+  },
+  {
+    baseForm: 'catch',
+    simplePast: 'caught',
+    pastParticiple: 'caught',
+  },
+  {
+    baseForm: 'choose',
+    simplePast: 'chose',
+    pastParticiple: 'chosen',
+  },
+  {
+    baseForm: 'cling',
+    simplePast: 'clung',
+    pastParticiple: 'clung',
+  },
+  {
+    baseForm: 'come',
+    simplePast: 'came',
+    pastParticiple: 'come',
+  },
+  {
+    baseForm: 'cost',
+    simplePast: 'cost',
+    pastParticiple: 'cost',
+  },
+  {
+    baseForm: 'creep',
+    simplePast: 'crept',
+    pastParticiple: 'crept',
+  },
+  {
+    baseForm: 'cut',
+    simplePast: 'cut',
+    pastParticiple: 'cut',
+  },
+  {
+    baseForm: 'deal',
+    simplePast: 'dealt',
+    pastParticiple: 'dealt',
+  },
+  {
+    baseForm: 'dig',
+    simplePast: 'dug',
+    pastParticiple: 'dug',
+  },
+  {
+    baseForm: 'do',
+    simplePast: 'did',
+    pastParticiple: 'done',
+  },
+  {
+    baseForm: 'draw',
+    simplePast: 'drew',
+    pastParticiple: 'drawn',
+  },
+  {
+    baseForm: 'drink',
+    simplePast: 'drank',
+    pastParticiple: 'drunk',
+  },
+  {
+    baseForm: 'drive',
+    simplePast: 'drove',
+    pastParticiple: 'driven',
+  },
+  {
+    baseForm: 'eat',
+    simplePast: 'ate',
+    pastParticiple: 'eaten',
+  },
+  {
+    baseForm: 'fall',
+    simplePast: 'fell',
+    pastParticiple: 'fallen',
+  },
+  {
+    baseForm: 'feed',
+    simplePast: 'fed',
+    pastParticiple: 'fed',
+  },
+  {
+    baseForm: 'feel',
+    simplePast: 'felt',
+    pastParticiple: 'felt',
+  },
+  {
+    baseForm: 'fight',
+    simplePast: 'fought',
+    pastParticiple: 'fought',
+  },
+  {
+    baseForm: 'find',
+    simplePast: 'found',
+    pastParticiple: 'found',
+  },
+  {
+    baseForm: 'flee',
+    simplePast: 'fled',
+    pastParticiple: 'fled',
+  },
+  {
+    baseForm: 'fling',
+    simplePast: 'flung',
+    pastParticiple: 'flung',
+  },
+  {
+    baseForm: 'fly',
+    simplePast: 'flew',
+    pastParticiple: 'flown',
+  },
+  {
+    baseForm: 'forbid',
+    simplePast: 'forbade',
+    pastParticiple: 'forbidden',
+  },
+  {
+    baseForm: 'forget',
+    simplePast: 'forgot',
+    pastParticiple: 'forgot, forgotten',
+  },
+];
+
+export default verbs;
