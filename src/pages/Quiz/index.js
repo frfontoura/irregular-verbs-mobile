af7f1165460799735a57c5209e7a8d49154bea1d
frfontoura <frfontoura@gmail.com>
2019-08-23 22:16:50 -0300

    Botão de next question do quiz

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { View, FlatList, StyleSheet } from 'react-native';
+import {
+  View, Text, TouchableOpacity, StyleSheet,
+} from 'react-native';
 
 import data from '~/assets/list_irreguar_verbs';
 
-import Item from '~/components/Item';
-
 export default function Quiz({ navigation }) {
   const [questions, setQuestions] = useState([]);
+  const [question, setQuestion] = useState({ });
+  const [number, setNumber] = useState(0);
 
   useEffect(() => {
     const qtd = navigation.getParam('questions', '10');
@@ -17,13 +19,30 @@ export default function Quiz({ navigation }) {
     setQuestions(Array.from(sorted));
   }, []);
 
+  useEffect(() => {
+    setQuestion(questions[number]);
+  }, [questions, number]);
+
+  function nextQuestion() {
+    if (number < questions.length) {
+      const qs = [...questions];
+      qs[number] = { ...question, test: 'ssdfas' };
+      setQuestions(qs);
+      setNumber(number + 1);
+    } else {
+      // final
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <FlatList
-        data={questions}
-        keyExtractor={item => `${item.baseForm}`}
-        renderItem={({ item }) => (<Item {...item} />)}
-      />
+      <View style={styles.questionContainer}>
+        <Text style={styles.text}>{question ? question.baseForm : ''}</Text>
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={() => nextQuestion()}>
+        <Text style={styles.buttonText}>Next</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -31,10 +50,31 @@ export default function Quiz({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
     paddingHorizontal: 20,
     backgroundColor: '#000',
   },
+  questionContainer: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+  },
   text: {
-    color: '#fff',
+    color: '#08f',
+    fontSize: 50,
+    alignSelf: 'center',
+  },
+  button: {
+    flexDirection: 'row',
+    padding: 15,
+    borderRadius: 3,
+    backgroundColor: '#fff',
+    marginBottom: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    marginLeft: 15,
   },
 });
