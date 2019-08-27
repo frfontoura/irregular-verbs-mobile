b8a0e59ed5677849997e2a3b588eadbb96a62521
frfontoura <frfontoura@gmail.com>
2019-08-26 21:28:02 -0300

    Adicionando os TextInput

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {
-  View, Text, TouchableOpacity, StyleSheet,
+  View, Text, TouchableOpacity, StyleSheet, TextInput,
 } from 'react-native';
 
 import data from '~/assets/list_irreguar_verbs';
@@ -9,6 +9,9 @@ export default function Quiz({ navigation }) {
   const [questions, setQuestions] = useState([]);
   const [question, setQuestion] = useState({ });
   const [number, setNumber] = useState(0);
+  const [simplePast, setSimplePast] = useState();
+  const [pastParticiple, setPastParticiple] = useState();
+  const [showResult, setShowResult] = useState(false);
 
   useEffect(() => {
     const qtd = navigation.getParam('questions', '10');
@@ -26,11 +29,19 @@ export default function Quiz({ navigation }) {
   function nextQuestion() {
     if (number < questions.length) {
       const qs = [...questions];
-      qs[number] = { ...question, test: 'ssdfas' };
+
+      qs[number] = {
+        ...question,
+        simplePastAnswer: simplePast,
+        pastParticipleAnswer: pastParticiple,
+      };
+
       setQuestions(qs);
       setNumber(number + 1);
+      setPastParticiple();
+      setSimplePast();
     } else {
-      // final
+      setShowResult(true);
     }
   }
 
@@ -38,9 +49,21 @@ export default function Quiz({ navigation }) {
     <View style={styles.container}>
       <View style={styles.questionContainer}>
         <Text style={styles.text}>{question ? question.baseForm : ''}</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Simple Past"
+          onChangeText={text => setSimplePast(text)}
+          value={simplePast}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Past Participle"
+          onChangeText={text => setPastParticiple(text)}
+          value={pastParticiple}
+        />
       </View>
 
-      <TouchableOpacity style={styles.button} onPress={() => nextQuestion()}>
+      <TouchableOpacity style={styles.button} onPress={() => nextQuestion()} disabled={!simplePast && !pastParticiple}>
         <Text style={styles.buttonText}>Next</Text>
       </TouchableOpacity>
     </View>
@@ -57,13 +80,14 @@ const styles = StyleSheet.create({
   },
   questionContainer: {
     flex: 1,
-    justifyContent: 'space-around',
+    justifyContent: 'center',
     alignItems: 'stretch',
   },
   text: {
     color: '#08f',
     fontSize: 50,
     alignSelf: 'center',
+    marginBottom: 10,
   },
   button: {
     flexDirection: 'row',
@@ -76,5 +100,13 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     marginLeft: 15,
+  },
+  textInput: {
+    height: 40,
+    backgroundColor: '#fff',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
